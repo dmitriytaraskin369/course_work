3b85c03dd0e01a1fb188c028b7978cdff4639340
Dmitriy Taraskin <dmit.taraskin@gmail.com>
2023-08-14 18:50:43 +0500

    add sorted function

--- utils.py
+++ utils.py
@@ -6,3 +6,13 @@ def load_operations(filename):
         data = json.load(f)
 
     return data
+
+
+def sort_operations_by_date(operations):
+    valid_operations = []
+    for operation in operations:
+        if operation.get('date'):
+            valid_operations.append(operation)
+
+    result = sorted(valid_operations, key=lambda operation: operation['date'], reverse=True)
+    return result
